7e374ec1513f6fb1818ab04051afc6bc5f1a3512
Brajul <132092432+Brajul-Rajput@users.noreply.github.com>
2025-10-28 16:44:04 +0530

    Create A3.java

---
+++ A3.java
@@ -0,0 +1,20 @@
+public class A3 { 
+    //data type ...
+    public static void main(String[] args) {
+        int a=10;
+        float  b=23.56f;
+        char c='A';
+        Boolean d=true;
+        double e=34.5645;
+        short f=34;
+        long g=1234567890;
+        System.out.println("Integer value: " + a);
+        System.out.println("Float value: " + b);
+        System.out.println("Character value: " + c);
+        System.out.println("Boolean value: " + d);
+        System.out.println("Double value: " + e);
+        System.out.println("Short value: " + f);
+        System.out.println("Long value: " + g);
+
+    }
+}
