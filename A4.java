40a0d6a4061de7af58d21ca9f9cb92e60a8ee395
Brajul <132092432+Brajul-Rajput@users.noreply.github.com>
2025-10-28 16:50:17 +0530

    Create A4.java

---
+++ A4.java
@@ -0,0 +1,8 @@
+public class A4 { 
+    // this is a simple program to add two numbers
+    public static void main(String[] args) {
+        int a=10;
+        int b=20;
+        System.out.println(a+b);
+    }
+}
