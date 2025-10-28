2609332b49e703dcb550f4a4efae61684823a837
Brajul <132092432+Brajul-Rajput@users.noreply.github.com>
2025-10-28 18:10:53 +0530

    Create A9.java

---
+++ A9.java
@@ -0,0 +1,12 @@
+public class A9 {
+   // logical operator
+    public static void main(String[] args) {
+        boolean x = true;
+        boolean y = false;
+
+        System.out.println("x && y: " + (x && y)); // false
+        System.out.println("x || y: " + (x || y)); // true
+        System.out.println("!x: " + (!x));           // false
+        System.out.println("!y: " + (!y));           // true
+    } 
+}
