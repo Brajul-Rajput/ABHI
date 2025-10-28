f549d1f13ce1df4d9f43de4654c8bb360d1cf76c
Brajul <132092432+Brajul-Rajput@users.noreply.github.com>
2025-10-28 18:26:34 +0530

    Create A11.java

---
+++ A11.java
@@ -0,0 +1,18 @@
+public class A11 {
+    // pre/post increment/decrement
+    public static void main(String[] args) {
+        int a=3;
+        int b=6;
+        System.out.println(a);  //3
+        System.out.println(a++);  //3
+       System.out.println(a);  //4
+        System.out.println(++a);  //5
+        System.out.println(a);  //5
+        System.out.println(b);   //6
+        System.out.println(b--);   //6
+        System.out.println(b);   // 5
+        System.out.println(--b);  //4
+        System.out.println(b);   //4
+
+    }
+}
