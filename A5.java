f6de962c84d453cc6d7273527fce970a6aadef1d
Brajul <132092432+Brajul-Rajput@users.noreply.github.com>
2025-10-28 16:58:53 +0530

    Create A5.java

---
+++ A5.java
@@ -0,0 +1,13 @@
+public class A5 {
+    //type casting 
+    public static void main(String[] args) {
+      //implicit type casting
+      int a=10;
+      double b=a;
+      System.out.println(b);
+        //explicit type casting
+        double c=1234.564;
+     int  d=(int )c; 
+        System.out.println(d);
+    }
+}
