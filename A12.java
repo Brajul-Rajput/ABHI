6caa5ed37f154492adb1622294eb87715c060cc9
Brajul <132092432+Brajul-Rajput@users.noreply.github.com>
2025-10-29 18:49:36 +0530

    Create A12.java

---
+++ A12.java
@@ -0,0 +1,11 @@
+public class A12 {
+    public static void main(String[] args) {
+        int a=5;
+        int b=7;
+        System.out.println(a&b); //5
+        System.out.println(a|b); //
+        System.out.println(a^b);  //2
+        System.out.println(~a);  //
+        System.out.println(~b); //
+    }
+}
