83a8cfb94231fd2af5dde7db705e780be3c90c43
Brajul <132092432+Brajul-Rajput@users.noreply.github.com>
2025-10-29 20:50:40 +0530

    Update A12.java

--- A12.java
+++ A12.java
@@ -5,7 +5,7 @@ public class A12 {
         System.out.println(a&b); //5
         System.out.println(a|b); //
         System.out.println(a^b);  //2
-        System.out.println(~a);  //
-        System.out.println(~b); //
+        System.out.println(~a);  //6
+        System.out.println(~b); //8
     }
 }
