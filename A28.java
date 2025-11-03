27a59ee911f2f04c0242d191137d8c9c629e4328
Brajul-Rajput <132092432+Brajul-Rajput@users.noreply.github.com>
2025-11-03 18:05:03 +0530

    Create A28.java

---
+++ A28.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class A28 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter the number: ");
+        int n=sc.nextInt();
+     sc.close();   
+    }
+}
