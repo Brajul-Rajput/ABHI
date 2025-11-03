6ee15a10472f6fd2594ea2ab447740caab9666e1
Brajul-Rajput <132092432+Brajul-Rajput@users.noreply.github.com>
2025-11-03 20:57:50 +0530

    Create A39.java

---
+++ A39.java
@@ -0,0 +1,12 @@
+import java.util.*;
+//Write a program to find the square of a number.
+public class A39 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter the number");
+        int n=sc.nextInt();
+        System.out.println("The square of the number is: "+(n*n));
+sc.close();
+        
+    }
+}
