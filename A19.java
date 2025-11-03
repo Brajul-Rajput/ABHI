3b7f9db8388c0bc6aa6cda825f4853b1b17561b2
Brajul-Rajput <132092432+Brajul-Rajput@users.noreply.github.com>
2025-11-03 16:44:03 +0530

    Create A19.java

---
+++ A19.java
@@ -0,0 +1,17 @@
+public class A19 {
+    public static void main(String[] args) {
+        demo19 obj=new demo19();
+        obj.display();
+    }
+}
+class demo19{
+    int a;
+    String b;
+    float c;
+
+    void display(){
+        System.out.println("a: "+a);
+        System.out.println("b: "+b);
+        System.out.println("c: "+c);
+    }
+}
