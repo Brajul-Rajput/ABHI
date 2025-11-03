2875d601f3badeb05c616896e2da10713d7a2df9
Brajul-Rajput <132092432+Brajul-Rajput@users.noreply.github.com>
2025-11-03 22:06:58 +0530

    Create A47.java

---
+++ A47.java
@@ -0,0 +1,8 @@
+public class A47 {
+    public static void main(String[] args) {
+        int leng=24;
+        int wid=10;
+        int area=leng*wid;
+        System.out.println("Area of rectangle is: "+area);
+    }
+}
