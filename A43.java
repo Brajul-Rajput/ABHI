539ed4f2b786bb5d19e4d8bcac85674e18dce804
Brajul-Rajput <132092432+Brajul-Rajput@users.noreply.github.com>
2025-11-03 21:29:57 +0530

    Create A43.java

---
+++ A43.java
@@ -0,0 +1,16 @@
+import java.util.*;
+//WAP to count number of digits in a number
+public class A43 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter the number");
+        int n=sc.nextInt();
+        int count=0;
+        while(n>0){
+            n=n/10;
+            count++;
+        }
+        System.out.println("number of digits:"+count);
+        sc.close();
+    }
+}
