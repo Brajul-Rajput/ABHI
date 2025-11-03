e171e3a00eea1b3441e8d5cecded3af423d1568f
Brajul-Rajput <132092432+Brajul-Rajput@users.noreply.github.com>
2025-11-03 20:57:44 +0530

    Create A37.java

---
+++ A37.java
@@ -0,0 +1,8 @@
+public class A37 {
+    public static void main(String[] args) {
+        System.out.println(" print all alphabets from a to z");
+        for(char ch='a'; ch<='z'; ch++) {
+            System.out.print(ch + " ");
+        }
+    }
+}
