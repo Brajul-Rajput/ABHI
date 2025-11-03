9f9d6300c79ff8eff39d78e10e5905617934ec63
Brajul-Rajput <132092432+Brajul-Rajput@users.noreply.github.com>
2025-11-03 16:43:59 +0530

    Create A18.java

---
+++ A18.java
@@ -0,0 +1,16 @@
+public class A18 {
+    public static void main(String[] args) {
+demo d=new demo();
+d.show();
+    }
+}
+ class demo{
+    int a=10;
+    String name="hello";
+     void show(){
+
+        System.out.println("in show");
+        System.out.println(a);
+        System.out.println(name);
+     }
+}
