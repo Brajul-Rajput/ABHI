0e1f2459065c663fde3ca6e98727699c594b181e
Brajul-Rajput <132092432+Brajul-Rajput@users.noreply.github.com>
2025-11-07 16:00:02 +0530

    Create B5.java

---
+++ B5.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class B5 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int arr[]={1,2,3,4,5};
+        System.out.println(Arrays.toString(arr));
+        System.out.println(Arrays.asList(arr));
+        sc.close();
+    }
+}
