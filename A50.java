047f0570f57929cde4522b34507a48528aba13e4
Brajul-Rajput <132092432+Brajul-Rajput@users.noreply.github.com>
2025-11-07 15:59:50 +0530

    Update A50.java

---
+++ A50.java
@@ -0,0 +1,11 @@
+public class A50 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        a = a + b; // a now becomes 30
+        b = a - b; // b becomes 10
+        a = a - b; // a becomes 20
+        System.out.println("a=" + a);
+        System.out.println("b=" + b);
+    }
+}
