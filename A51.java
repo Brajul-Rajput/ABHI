1a869e8eb30c506765a5a08b82a7165190bd58ca
Brajul-Rajput <132092432+Brajul-Rajput@users.noreply.github.com>
2025-11-26 16:41:14 +0530

    Create A51.java

---
+++ A51.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class A51 {
+    public static void main(String[] args) {
+        List<Integer>list=new ArrayList<>();
+list.add(10);
+System.out.println(list.size());
+    }
+}
