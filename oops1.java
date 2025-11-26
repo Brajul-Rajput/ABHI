e7c0616cdede238826a71ca0e56fa2b802eab2c2
Brajul-Rajput <132092432+Brajul-Rajput@users.noreply.github.com>
2025-11-26 16:41:51 +0530

    Create oops1.java

---
+++ oops1.java
@@ -0,0 +1,14 @@
+public class oops1 {
+    public static void main(String[] args) {
+demo obj=new demo();
+obj.displ();
+    }
+}
+class demo{
+    int a=10;
+    String name="john";
+ void displ(){
+        System.out.println("a: "+a);
+        System.out.println("name: "+name);
+    }
+}
